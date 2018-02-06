45606d531677ce64f26d4faf00e4c17a3ba46fe5
LA Roberto <me@laroberto.com>
2018-02-06 13:58:32 +0800

    Add Redux for state management

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './App';
+import reducers from './reducer';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 registerServiceWorker();
