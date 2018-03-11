851187e7cd11677db82635d53f0aec3da7cee72a
LA Roberto <me@laroberto.com>
2018-03-11 22:15:05 +0800

    Add git command to action lookup

--- src/redux/modules/gitEmulator.js
+++ src/redux/modules/gitEmulator.js
@@ -4,3 +4,14 @@ export const gitAdd = fileName => ({
   type: GIT_ADD,
   fileName
 });
+
+const gitCommandActionLookup = {
+  add: gitAdd
+};
+
+export const executeCommand = input => dispatch => {
+  // TODO: add check if command does not exist
+  const { command, argument, flags } = input;
+  const gitCommand = gitCommandActionLookup[command];
+  dispatch(gitCommand(argument, flags));
+};
