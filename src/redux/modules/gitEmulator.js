18172e3cb2be4806d8690ea22c5007238334de61
LA Roberto <me@laroberto.com>
2018-03-13 09:57:08 +0800

    Add basic git add execution

--- src/redux/modules/gitEmulator.js
+++ src/redux/modules/gitEmulator.js
@@ -1,13 +1,39 @@
-import { addLogInfo } from './terminal';
+import { addLogInfo, addLogError } from './terminal';
 import { commandRebuilder } from '../../utils/commandProcessor';
+import { getFiles } from './fileSystem';
 
 export const GIT_ADD = 'modules/gitEmulator/GIT_ADD';
 
 export const gitAdd = fileName => ({
   type: GIT_ADD,
-  fileName
+  fileName,
 });
+
+const emulateAdd = ({ command, argument, flags }) => (dispatch, getState) => {
+  const files = getFiles(getState());
+  if (files.includes(argument)) {
+    return dispatch(gitAdd(argument));
+  }
+  return dispatch(
+    addLogError(`fatal: pathspec '${argument}' did not match any files`)
+  );
+};
+
+const commandActionLookup = {
+  add: emulateAdd,
+};
 
 export const executeCommand = input => dispatch => {
   dispatch(addLogInfo(commandRebuilder(input)));
+  const availableCommands = Object.keys(commandActionLookup);
+
+  if (input.executable !== 'git') {
+    return dispatch(addLogError(`command not found: ${input.executable}`));
+  }
+
+  if (availableCommands.includes(input.command)) {
+    return dispatch(commandActionLookup[input.command](input));
+  }
+
+  return dispatch(addLogError(`git: '${input.command}' is not a git command.`));
 };
